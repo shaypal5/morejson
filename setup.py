39dd0da17997ba9d89a83f1616408df79193be6d
Shay Palachy <shaypal5@gmail.com>
2017-03-03 13:54:12 +0200

    fixing setup files

--- setup.py
+++ setup.py
@@ -6,22 +6,52 @@
 from setuptools import setup, find_packages
 import versioneer
 
+README_RST = ''
 with open('README.rst') as f:
-    README = f.read()
+    README_RST = f.read()
 
 setup(
-    author="Shay Palachy",
-    author_email="shaypal5@gmail.com",
     name='morejson',
-    description="morejson is a drop-in replacement for Python's json module "
-                "that handles additional built-in and standard library Python"
-                " types.",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    long_description=README,
+    description="Wraps Python json, supporting to more Python built-in types.",
+    long_description=README_RST,
+    license='MIT',
+    author="Shay Palachy",
+    author_email="shaypal5@gmail.com",
     url='https://github.com/shaypal5/morejson',
-    packages=find_packages(),
+    packages=['morejson'],
+    # packages=find_packages(),
     install_requires=[],
     test_suite='nose.collector',
     tests_require=['nose'],
+    platforms=['any'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: SunOS/Solaris',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Topic :: Other/Nonlisted Topic',
+        'Intended Audience :: Developers',
+    ],
+    keywords='json datetime timedelta timezone frozenset',
 )
